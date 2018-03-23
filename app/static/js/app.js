abffe47e9975436c6d4f2398bcfaf6ec58492ef9
Javed Wright <jdwright870@gmail.com>
2018-03-23 20:18:21 +0000

    added article images, title and description

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -43,14 +43,15 @@ Vue.component('app-footer', {
 Vue.component('news-list', {
     template: `<div class="news">
                     <h2>News</h2>
-                    <ul class="news__list">
-                        <li class="news__item">News item 1</li>
-                        <li class="news__item">News item 2</li>
-                        <li class="news__item">News item 3</li>
-                    </ul>
+                    <div v-for="article in articles" class="news__item">
+                        <img v-bind:src="article.urlToImage"/>
+                        <label>{{article.title}}</label>
+                        <p>{{article.description}}</p>
+                    </div>
                 </div>
                 `,
     created: function(){
+        let self = this;
         let api_key = '';
         fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${api_key}`)
         .then(function(response) {
@@ -58,7 +59,13 @@ Vue.component('news-list', {
         })
         .then(function(data) {
             console.log(data);
+            self.articles = data.articles;
         });
+    },
+    data: function(){
+        return {
+            articles: []
+        }
     }
 })
 
